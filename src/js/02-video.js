3494d86ca29f527af64c245e7664959a602f9b42
MariiaZhk <maria8zhuk@gmail.com>
2023-11-08 18:28:57 +0100

    first-commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+// HTML містить <iframe> з відео для Vimeo плеєра. Напиши скрипт, який буде зберігати поточний час відтворення відео у локальне сховище і, після перезавантаження сторінки, продовжувати відтворювати відео з цього часу.
+// Ознайомся з документацією бібліотеки Vimeo плеєра.
+// Додай бібліотеку як залежність проекту через npm.
+// Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player, але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
+// Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
+// Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
+// Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+// Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const getCurrentTime = function (currentTime) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(currentTime.seconds));
+};
+
+const currentTimeLS = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+player
+  .setCurrentTime(currentTimeLS)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
+// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
 
+import throttle from 'lodash.throttle';
+
+const feedbackFormEl = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const userFeedback = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+const fillFeedbackFormField = () => {
+  const userFeedbackFromLS = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (userFeedbackFromLS === null) {
+    return;
+  }
+  for (const key in userFeedbackFromLS) {
+    if (userFeedbackFromLS.hasOwnProperty(key)) {
+      feedbackFormEl.elements[key].value = userFeedbackFromLS[key];
+    }
+  }
+};
+
+fillFeedbackFormField();
+
+const onFeedbackFormFieldInput = ({ target: feedbackFormField }) => {
+  const feedbackFormFieldValue = feedbackFormField.value;
+  const feedbackFormFieldName = feedbackFormField.name;
+
+  userFeedback[feedbackFormFieldName] = feedbackFormFieldValue;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(userFeedback));
+};
+
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+  console.log(userFeedback);
+  feedbackFormEl.reset();
+  localStorage.removeItem(STORAGE_KEY);
+};
+
+console.log(userFeedback);
+
+feedbackFormEl.addEventListener(
+  'input',
+  throttle(onFeedbackFormFieldInput, 500)
+);
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
